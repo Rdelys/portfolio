285f93e32c0b4cf85ccbd04b7263ef2f7dccd8a2
Rdelys <rdauphinelys@gmail.com>
2026-01-01 01:22:42 +0300

    fin du style du navbar

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-function loadComponent(id, path) {
+function loadComponent(id, path, callback) {
     fetch(path)
         .then(res => res.text())
         .then(data => {
             document.getElementById(id).innerHTML = data;
+            if (callback) callback(); // ⚡ exécute le JS après injection
         });
 }
 
-loadComponent("navbar", "components/navbar/navbar.html");
+loadComponent("navbar", "components/navbar/navbar.html", window.initNavbar);
 loadComponent("hero", "components/hero/hero.html");
 loadComponent("services", "components/services/services.html");
 loadComponent("tecno", "components/tecno/tecno.html");
